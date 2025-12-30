59ba10f5ae8e93458708f27d070d121ab7ba7624
Mustang Thumbi <thumbiwvictor@gmail.com>
2025-12-30 13:56:22 +0300

    chore - opening select modal on the navbar

--- console/v1/src/components/Navbar.vue
+++ console/v1/src/components/Navbar.vue
@@ -49,11 +49,86 @@
 
         <!-- Right: account / help / cart -->
         <v-col class="ga-3">
-          <v-btn class="ga-2 hover-primary">
+          <!-- <v-btn class="ga-2 hover-primary">
             <v-icon size="32">mdi-account-outline</v-icon>
             Account
             <v-icon size="24">mdi-chevron-down</v-icon>
-          </v-btn>
+          </v-btn> -->
+          <v-menu
+  location="bottom end"
+  offset="8"
+  transition="scale-transition"
+>
+  <template #activator="{ props }">
+    <v-btn
+      v-bind="props"
+      class="ga-2 hover-primary"
+      variant="text"
+    >
+      <v-icon size="32">mdi-account-outline</v-icon>
+      Account
+      <v-icon size="24">mdi-chevron-down</v-icon>
+    </v-btn>
+  </template>
+
+  <!-- Dropdown Card -->
+  <v-card
+    min-width="240"
+    elevation="8"
+    rounded="lg"
+  >
+
+      <!-- Header -->
+    <v-list-item class="py-3">
+      <template #prepend>
+        <v-avatar color="grey-lighten-3">
+          <v-icon>mdi-account</v-icon>
+        </v-avatar>
+      </template>
+
+      <v-list-item-title class="font-weight-medium">
+        Hi, victor
+      </v-list-item-title>
+    </v-list-item>
+
+    <v-divider />
+
+    <!-- Menu Items -->
+    <v-list density="compact" nav>
+      <v-list-item
+        v-for="item in menuItems"
+        :key="item.title"
+        :to="item.to"
+        link
+      >
+        <template #prepend>
+          <v-icon>{{ item.icon }}</v-icon>
+        </template>
+
+        <v-list-item-title>
+          {{ item.title }}
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+
+    <v-divider />
+
+    <!-- Logout -->
+    <v-list-item
+      class="text-center logout-item"
+      @click="logout"
+    >
+      <v-list-item-title class="text-orange font-weight-medium">
+        Logout
+      </v-list-item-title>
+    </v-list-item>
+  </v-card>
+</v-menu>
+
+
+
+
+
 
           <v-btn text class="pa-1 hover-primary">
             <v-icon size="26">mdi-help-circle-outline</v-icon>
@@ -84,6 +159,19 @@ function onSearch() {
   // simple behavior: navigate to search or console log for now
   // This can be replaced with proper search routing later
   console.log("Search:", query.value);
+}
+
+const menuItems = [
+  { title: 'My Account', icon: 'mdi-account-outline', to: '/account' },
+  { title: 'Orders', icon: 'mdi-package-variant', to: '/orders' },
+  { title: 'Inbox', icon: 'mdi-email-outline', to: '/inbox' },
+  { title: 'Wishlist', icon: 'mdi-heart-outline', to: '/wishlist' },
+  { title: 'Vouchers', icon: 'mdi-ticket-percent-outline', to: '/vouchers' }
+]
+
+const logout = () => {
+  // Supabase / Auth logout here
+  console.log('Logged out')
 }
 </script>
 
